edfd93706cd832b0b38e178850cd924400e0ed97
innocent <innocentgodwin580@yahoo.com>
2025-07-21 14:37:24 +0930

    nat gateway created

--- main.tf
+++ main.tf
@@ -21,3 +21,22 @@ module "vpc" {
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 
 }
+
+
+# create nat gateway 
+module "nat_gateway" {
+  source = "git@github.com:Godwin-svg/terraform-modules.git//modules/nat-gateway"
+
+  project_name = local.project_name
+  environment  = local.environment
+
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  internet_gateway           = module.vpc.internet_gateway
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+
+}
